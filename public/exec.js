a739e69d3ae24c673f0c6fb1c6b59c372e8bab48
NghiepPT <32757821+NghiepPT@users.noreply.github.com>
2018-06-04 15:26:34 +0700

    make it better

--- public/exec.js
+++ public/exec.js
@@ -1,24 +1,75 @@
-var socket = io("https://chatchitvn.herokuapp.com/")
+var socket = io("https://chatchitvn.herokuapp.com/");
 //var socket = io("http://localhost:50000/");
+var currentUser;
 socket.on("server-send-reg-fail", function () {
     alert("User name is exist");
 });
-socket.on("server-send-reg-success", function (data) {   
-    $("#loginForm").hide();
-    $("#chatForm").hide();
-    $("#txtGroup").show();
+socket.on("server-send-reg-success", function (data) {  
+
+    viewform("REGISTER_SUCCESS");
     $("#msg").html(data);
 });
 
-socket.on("server-send-online-users", function (data) {
-    $("#boxtContent").html("");
+socket.on("server-send-friend-list", function(data){
+    $("#listFriend").html("");
+    data.forEach(function(i){
+        $("#listFriend").append("<div class='frd'>"
+        +"<img class= 'avatar' align='left' src='https://avatar.skype.com/v1/avatars/pham.nghiep87?auth_key=-1911296691&returnDefaultImage=false&cacheHeaders=true'></img>"
+        +"<div class='user'>" + i.myfriend + "</div>" + "<div id='status'>Offline</div> "+"</div>");    
+    }); 
+ 
+});
+socket.on("server-send-online-users", function (data) { 
+    console.log($(".user").val())   ;
     data.forEach(function (i) {
-        $("#boxtContent").append("<div class='user'>" + i + "</div>");
+       $("#listFriend").find('.frd').each(function(){
+           var isOnline = false;
+           var frd = $(this);
+           frd.find('.user').each(function(){    
+               var usr = $(this);
+             if(usr.text() == i)
+             {
+                frd.find('#status').each(function(){
+                  
+                  usr.css('color','#4ccc16');
+                  $(this).html("");
+                  $(this).append("Online");               
+                    
+                 });
+             }
+         });  
+       })
+    });
+});
+
+socket.on("server-send-user-off", function (data) { 
+    console.log($(".user").val())   ;
+    data.forEach(function (i) {
+       $("#listFriend").find('.frd').each(function(){
+           var isOnline = false;
+           var frd = $(this);
+           frd.find('.user').each(function(){    
+               var usr = $(this);
+             if(usr.text() == i)
+             {
+                frd.find('#status').each(function(){
+                  
+                  usr.css('color','#c4ccc1');
+                  $(this).html("");
+                  $(this).append("Offline");
+                 // $(this).css('color','#c4ccc1');
+                    
+                 });
+             }
+         });  
+       })
     });
 });
 
 socket.on("server-send-message", function (data) {
-    $("#listMessage").append("<div class='message'>" + data + "</div>");
+    $("#listMessage").append("<div class='message'>" + "<img class= 'avatar' src='https://avatar.skype.com/v1/avatars/pham.nghiep87?auth_key=-1911296691&returnDefaultImage=false&cacheHeaders=true'/></img>"+"<div class='name'>" + data.usr + 
+    "</div>" + "<div class='content'>" 
+    + data.coversation + "</div>" + "</div>");
 });
 
 socket.on("user-typing-message", function(data){
@@ -30,49 +81,106 @@ socket.on("user-leave-typing", function(){
 });
 
 socket.on("server-send-login-success", function(data){
-    $("#loginForm").hide();
-    $("#chatForm").show();
-    $("#currentUser").html(data);
+    init();
+    viewform("LOGIN_SUCCESS");
+    $("#displayname").html(data);
+    currentUser = data;
 });
-socket.on("server-send-user-online",function(data){
-    $("#boxtContent").html("");
-    data.forEach(function (i) {
-        $("#boxtContent").append("<div class='user'>" + i + "</div>");
-    });
-});
+
 socket.on("server-send-login-fail", function(){
-   // $("#loginForm").show();
+   // $("#ctrl-form").show();
     $("#txtSgFail").show();
 });
-socket.on("server-send-user-off", function(data){
-    $("#boxtContent").html("");
-    data.forEach(function (i) {
-        $("#boxtContent").append("<div class='user'>" + i + "</div>");
-    });
-});
+
 function init() {
+    
+    $("#ctrl-form").hide();
     $("#txtGroup").hide();
     $("#chatForm").hide();
     $("#regForm").hide();
     $("#btnReg").hide();
+    $("#btnSignIn").hide();
+    $("#btnSignUp").hide();
+    $("#btnAdd").hide();
     $("#eml").hide();
-    $("#txtSgFail").hide();
+    $("#usr").hide();
+    $("#pwd").hide();
+    $("#txtSgFail").hide(); 
+    $(".no-account").hide();
+}
+
+function initfield(){
+    $(".text-title").html("");
+    $("#eml").val("");
+    $("#usr").val("");
+    $("#pwd").val("");
+}
+
+function viewform(view_state)
+{
+   
+    switch(view_state)
+    {
+        case "LOGIN":
+        {
+            $("#ctrl-form").show();
+            $("#btnSignIn").show();
+            $("#usr").show();
+            $("#pwd").show();
+            $(".no-account").show();
+            break;
+        }
+        case "LOGIN_SUCCESS":
+        {
+            $("#chatForm").show();
+            break;
+        }
+        case "LOGIN_FAIL":
+        {
+            $("#ctrl-form").show();
+            $("#btnSignIn").show();
+            $("#txtSgFail").show();
+            break;
+        }
+        case "REGISTER":
+        {
+            $("#ctrl-form").show();
+            $("#btnReg").show();
+            $("#eml").show();
+            $("#usr").show();
+            $("#pwd").show();
+            break;  
+        }
+        case "REGISTER_SUCCESS":
+        {
+            $("#ctrl-form").hide();
+            $("#chatForm").hide();
+            $("#txtGroup").show();
+            break;
+        }
+        default:
+            init();
+        break;
+    } 
+
 }
 $(document).ready(function () {
-
-   // $("#loginForm").show();
+ 
   
    init();
-
+   viewform("LOGIN");   
     $("#btnSgOut").click(function () {
         socket.emit("client-send-sign-out");
-        $("#loginForm").show();
+        $("#ctrl-form").show();
         $("#chatForm").hide();
     });
 
-    $("#btnSend").click(function () {
-        socket.emit("user-send-message", $("#txtMessage").val());
-        $("#txtMessage").val("");
+    $("#msgBox").keyup(function (event) {
+        if(event.keyCode == 13)
+        {
+            socket.emit("user-send-message", $("#msgBox").val());
+            $("#msgBox").val("");
+        }
     });
 
     $("#btnSignUp").click(function(){  
@@ -82,7 +190,7 @@ $(document).ready(function () {
         $("#btnSignUp").hide();
         $(".email").show();
     });
-
+ 
     $("#btnSignIn").click(function(){
         var info ={usr: $("#usr").val(), pwd:$("#pwd").val()}
         socket.emit("client-send-login-request",info);
@@ -92,12 +200,24 @@ $(document).ready(function () {
         socket.emit("send-register-form", info);
     });
 
+    $(".register").click(function(){
+        init();
+        initfield();      
+        $(".text-title").append("<h4>Create account</h4>");
+        viewform("REGISTER");
+    });
+    $(".icon-add-friend").click(function(){
+        init();
+        $("#ctrl-form").show();
+        $("#btnAdd").show();   
+        $("#usr").show();   
+    });
+    
     $("#txtMessage").focusin(function(){
         socket.emit("user-send-typing-message");
     });
     $("#txtMessage").focusout(function(){
         socket.emit("user-send-leave-typing");
     });
-
 
 });
--- server.js
+++ server.js
@@ -8,97 +8,106 @@ app.set("views", "./views");
 
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(process.env.PORT||50000);
+server.listen(process.env.PORT || 50000);
 
-var MongoClient =  mongodb.MongoClient;
+var MongoClient = mongodb.MongoClient;
 
-var url ='mongodb://PhamNghiep:thaiduonghe123@ds014118.mlab.com:14118/mdb';
+var url = 'mongodb://PhamNghiep:thaiduonghe123@ds014118.mlab.com:14118/mdb';
 
 
-var onUsers=[];
-io.on("connection", function(socket){
+var onUsers = [];
+var OffUsr = [];
+io.on("connection", function (socket) {
 
-console.log("client is connecting..");
+    console.log("client is connecting..");
 
-socket.on("send-register-form", function(data){
-    console.log("client send register form");
-    MongoClient.connect(url, function(err, db){
-        if(err){
-            throw err;
-            db.close();
-        }
-        else{
-           var dbo = db.db("mdb");
-           dbo.collection("user").findOne(data, function(err, result){
-               if(err) throw err;
-               else{
-                   if(result)
-                   {   
-                    socket.emit("server-send-reg-fail");
-                   }
-                   else
-                   {
-                    dbo.collection("user").insertOne(data, function(err, res){
-                        if(err) throw err;
-                        db.close();
-                    });
-                    socket.emit("server-send-reg-success","You register successfully!");
-                   } 
-               }
-               db.close();
-           });
-
-        }
-    });  
-});
-socket.on("client-send-login-request", function(data){
-    MongoClient.connect(url,function(err, db){
-        if(err) throw err;
-        else{
-            var dbo = db.db("mdb");
-            dbo.collection("user").findOne(data, function(err, result){
-                if(err) throw err;
-                else{
-                    if(result.usr){                     
-                        if(onUsers.indexOf(result.usr< 0))
-                        {
-                            onUsers.push(result.usr);
-                            socket.UserName = result.usr;
+    socket.on("send-register-form", function (data) {
+        console.log("client send register form");
+        MongoClient.connect(url, function (err, db) {
+            if (err) {
+                throw err;
+                db.close();
+            }
+            else {
+                var dbo = db.db("mdb");
+                dbo.collection("user").findOne(data, function (err, result) {
+                    if (err) throw err;
+                    else {
+                        if (result) {
+                            socket.emit("server-send-reg-fail");
                         }
-                        socket.emit("server-send-login-success",result.usr);
-                        io.sockets.emit("server-send-user-online",onUsers);
+                        else {
+                            dbo.collection("user").insertOne(data, function (err, res) {
+                                if (err) throw err;
+                                db.close();
+                            });
+                            socket.emit("server-send-reg-success", "You register successfully!");
+                        }
                     }
-                    else{
-                        socket.emit("server-send-login-fail");
+                    db.close();
+                });
+
+            }
+        });
+    });
+    socket.on("client-send-login-request", function (data) {
+        var onFriend = [];
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            else {
+                var dbo = db.db("mdb");
+                dbo.collection("user").findOne(data, function (err, result) {
+                    if (err) throw err;
+                    else {
+                        if (result) {
+                            if (onUsers.indexOf(result.usr) < 0) {
+                                onUsers.push(result.usr);
+                                socket.UserName = result.usr;
+                            }
+                            socket.emit("server-send-login-success", result.usr);
+                            dbo.collection("MyFriend").find({ myname: result.usr }, { _id: 0, myname: 0, myfriend: 1 }).toArray(function (err, res) {
+                                if (err) throw err;
+                                else {
+                                    if (res) {
+                                        socket.emit("server-send-friend-list", res);
+                                        io.sockets.emit("server-send-online-users", onUsers);
+                                    }
+                                }
+                            });
+
+
+                        }
+                        else {
+                            socket.emit("server-send-login-fail");
+                        }
                     }
-                }
-            });
+                });
+            }
+        });
+    });
+ 
+    socket.on("user-send-message", function (data) {
+        var msg = { usr: socket.UserName, coversation: data };
+        //save to database
+        io.sockets.emit("server-send-message", msg);
+    });
+
+    socket.on("user-send-typing-message", function () {
+        console.log("typing message");
+        socket.broadcast.emit("user-typing-message", { un: socket.UserName, content: "is typing" });
+    });
+    socket.on("user-send-leave-typing", function () {
+        socket.broadcast.emit("user-leave-typing");
+    });
+    socket.on("disconnect", function () {
+        if (OffUsr.indexOf(socket.UserName) < 0) {
+            OffUsr.push(socket.UserName);
         }
-    }) ;
-});
-socket.on("client-send-sign-out", function(){
-  console.log(socket.UserName);
-  onUsers.splice(onUsers.indexOf(socket.UserName),1);
-  socket.broadcast.emit("server-send-user-off",onUsers);
-  console.log(onUsers);
-});
-
-socket.on("user-send-message", function(data){
- io.sockets.emit("server-send-message", socket.UserName + ": " + data);
-});
-
-socket.on("user-send-typing-message", function(){
-    console.log("typing message");
-    socket.broadcast.emit("user-typing-message", {un: socket.UserName, content: "is typing"});
-});
-socket.on("user-send-leave-typing", function(){
-    socket.broadcast.emit("user-leave-typing");
-});
-socket.on("disconnect", function(){
+        onUsers.splice(onUsers.indexOf(socket.UserName), 1);
+        socket.broadcast.emit("server-send-user-off", OffUsr);
+    });
 
 });
-
-});
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("homepage");
 });
